8d4300b54ad2474d46ad2fec166326369c19f865
SamieQamar <93860180+Samie-Qamar@users.noreply.github.com>
2024-11-08 00:19:56 +0500

    installtion of Spatie PAckage for laravel

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Role;
+use Illuminate\Support\Facades\Validator;
+
+class RoleController extends Controller
+{
+    public function index()
+    {
+        return view();
+    }
+}
